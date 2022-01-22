cc2a61acff6275cc40b06e040357dbc5503568db
tituspijean <tituspijean@outlook.com>
2022-01-22 14:32:18 +0000

    Fixing dependencies for ARM64

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,7 +16,12 @@ architecture=$(dpkg --print-architecture)
 discovery_service_port=1900
 discovery_client_port=7359
 
-ffmpeg_deps="libass9 libbluray2 libc6 libdrm-intel1 libdrm2 libfontconfig1 libfreetype6 libfribidi0 libgcc1 libgmp10 libgnutls30 libmp3lame0 libopus0 libstdc++6 libtheora0 libvdpau1 libvorbis0a libvorbisenc2 libvpx5 libwebp6 libwebpmux3 libx11-6 libx264-155 libx265-165 libzvbi0 libopencl1 zlib1g"
+if [ $architecture == "arm64" ]; then
+    ffmpeg_deps="libass9 libbluray2 libc6 libdrm2 libfontconfig1 libfreetype6 libfribidi0 libgcc1 libgmp10 libgnutls30 libmp3lame0 libopus0 libstdc++6 libtheora0 libvdpau1 libvorbis0a libvorbisenc2 libvpx5 libwebp6 libwebpmux3 libx11-6 libx264-155 libx265-165 libzvbi0 zlib1g"
+else
+    ffmpeg_deps="libass9 libbluray2 libc6 libdrm-intel1 libdrm2 libfontconfig1 libfreetype6 libfribidi0 libgcc1 libgmp10 libgnutls30 libmp3lame0 libopus0 libstdc++6 libtheora0 libvdpau1 libvorbis0a libvorbisenc2 libvpx5 libwebp6 libwebpmux3 libx11-6 libx264-155 libx265-165 libzvbi0 libopencl1 zlib1g"
+fi
+
 jellyfin_deps="at libsqlite3-0 libfontconfig1 libfreetype6 libssl1.1"
 pkg_dependencies="$ffmpeg_deps $jellyfin_deps"
 
