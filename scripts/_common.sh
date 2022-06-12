d124083ec097fbdadd7584cf86b7b59c1ff2f6c6
Félix Piédallu <felix@piedallu.me>
2022-06-12 20:30:12 +0200

    Fix: ffmpeg -> ffmpeg5

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -100,7 +100,7 @@ install_jellyfin_packages() {
     # but the code says multiple calls are supported.
     # Also, they're already installed so that should be quasi instantaneous.
     ynh_install_app_dependencies \
-        jellyfin-ffmpeg="$ffmpeg_pkg_version-$debian" \
+        jellyfin-ffmpeg5="$ffmpeg_pkg_version-$debian" \
         jellyfin-server="$pkg_version" \
         jellyfin-web="$pkg_version"
 }
