7acbfbe2ad83dcead2340968be9dbeeac3095df1
Félix Piédallu <felix@piedallu.me>
2023-12-08 20:56:58 +0100

    Update ffmpeg to 6.0.1-1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ debian=$(lsb_release --codename --short)
 pkg_version="10.8.11-1"
 version=$(echo "$pkg_version" | cut -d '-' -f 1)
 
-ffmpeg_pkg_version="6.0-6"
+ffmpeg_pkg_version="6.0.1-1"
 ldap_pkg_version="16.0.0.0"
 
 discovery_service_port=1900
