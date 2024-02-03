6a3fa81973b169d6cf5e2c80d131f3e7cc252eb1
eric_G <46165813+ericgaspar@users.noreply.github.com>
2024-02-03 18:51:16 +0100

    Testing (#143)
    * Manifestv2
    
    * add back user and install_dir to manifest
    
    * Update manifest.toml
    
    * rewrite update_version.sh in python for toml
    
    * Bump jellyfin-ffmpeg to 6.0-6 as #136 (Thanks @Tagadda)
    
    * Only add user to render group if not already
    
    * Remove line_match from ynh_systemd_actions : they actually just match when systemctl returns
    
    * Fix upgrade: uninstall previous packages
    
    * Installation of deb packages will overwrite the configuration files, so to prevent a warning we backup those files beforehand.
    
    * Test with a long sleep
    
    * Auto-update README
    
    * update_version: fix linter
    
    * Add ldap to update_version.py
    
    * Revert sleep 30 from debugging
    
    * Do not remove packages during upgrade
    
    * Fix upgrade again
    
    ---------
    
    Co-authored-by: Félix Piédallu <felix@piedallu.me>
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,94 +8,81 @@ debian=$(lsb_release --codename --short)
 pkg_version="10.8.11-1"
 version=$(echo "$pkg_version" | cut -d '-' -f 1)
 
-ffmpeg_pkg_version="5.1.2-6"
+ffmpeg_pkg_version="6.0-6"
 ldap_pkg_version="16.0.0.0"
 
 discovery_service_port=1900
 discovery_client_port=7359
 
-ffmpeg_deps=(
-	libass9
-	libbluray2
-	libc6
-	libdrm2
-	libfontconfig1
-	libfreetype6
-	libfribidi0
-	libgcc1
-	libgmp10
-	libgnutls30
-	libllvm13
-	libmp3lame0
-	libopus0
-	libstdc++6
-	libtheora0
-	libvdpau1
-	libvorbis0a
-	libvorbisenc2
-	libwebp6
-	libwebpmux3
-	libx11-6
-	libxcb-randr0
-	libzvbi0
-	zlib1g
-)
+# Those directories are handled by the deb package
+data_path="/var/lib/$app"
+config_path="/etc/$app"
+log_path="/var/log/$app"
+cache_path="/var/cache/$app"
 
-case "$debian" in
-	buster) ffmpeg_deps+=( libvpx5 libx264-155 libx265-165 ) ;;
-	bullseye) ffmpeg_deps+=( libvpx6 libx264-160 libx265-192 ) ;;
-	*) echo "Unknown release: $debian" >&2; exit 1 ;;
-esac
-case "$YNH_ARCH" in
-	arm64) : ;;
-	armhf) : ;;
-	*) ffmpeg_deps+=( libdrm-intel1 libopencl1 ) ;;
-esac
-
-jellyfin_deps=(at libsqlite3-0 libfontconfig1 libfreetype6 libssl1.1)
-
-pkg_dependencies="${ffmpeg_deps[*]} ${jellyfin_deps[*]}"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
 install_jellyfin_packages() {
-	# In case of a new version, the url change from
-	# https://repo.jellyfin.org/releases/server/debian/versions/stable/server/X.X.X/jellyfin-server_X.X.X-1_$YNH_ARCH.deb to
-	# https://repo.jellyfin.org/archive/debian/stable/X.X.X/server/jellyfin-server_X.X.X-1_$YNH_ARCH.deb
-	for pkg in web server; do
-		src_url=$(grep 'SOURCE_URL=' "$YNH_APP_BASEDIR/conf/$pkg.$debian.$YNH_ARCH.src" | cut -d= -f2-)
-		if ! curl --output /dev/null --silent --head --fail "$src_url"; then
-			ynh_replace_string \
-				--match_string="releases/server/debian/versions/stable/$pkg/$version/" \
-				--replace_string="archive/debian/stable/$version/$pkg/" \
-				--target_file="$YNH_APP_BASEDIR/conf/$pkg.$debian.$YNH_ARCH.src"
-		fi
-	done
-
 	# Create the temporary directory
 	tempdir="$(mktemp -d)"
 
 	# Download the deb files
-	ynh_setup_source --dest_dir=$tempdir --source_id="ffmpeg.$debian.$YNH_ARCH"
-	ynh_setup_source --dest_dir=$tempdir --source_id="server.$debian.$YNH_ARCH"
-	ynh_setup_source --dest_dir=$tempdir --source_id="web.$debian.$YNH_ARCH"
+	ynh_setup_source --dest_dir="$tempdir" --source_id="web"
+	ynh_setup_source --dest_dir="$tempdir" --source_id="ffmpeg_$debian"
+	ynh_setup_source --dest_dir="$tempdir" --source_id="server"
 
 	# Install the packages
-	ynh_exec_warn_less dpkg --force-confdef --force-confnew -i $tempdir/jellyfin-web.deb
-	ynh_exec_warn_less dpkg --force-confdef --force-confnew -i $tempdir/jellyfin-ffmpeg5.deb
-	ynh_exec_warn_less dpkg --force-confdef --force-confnew -i $tempdir/jellyfin-server.deb
+	ynh_package_install \
+		"$tempdir/jellyfin-web.deb" \
+		"$tempdir/jellyfin-server.deb"
 
-	ynh_secure_remove --file="$tempdir"
+	# We need to workaround yunohoost passing --no-remove to replace jellyfin-ffmpeg5...
+	if ynh_package_is_installed "jellyfin-ffmpeg5"; then
+		ynh_package_remove "jellyfin-ffmpeg5"
+	fi
+	ynh_package_install \
+		"$tempdir/jellyfin-ffmpeg6.deb"
 
 	# The doc says it should be called only once,
 	# but the code says multiple calls are supported.
 	# Also, they're already installed so that should be quasi instantaneous.
 	ynh_install_app_dependencies \
 		jellyfin-web="$pkg_version" \
-		jellyfin-ffmpeg5="$ffmpeg_pkg_version-$debian" \
+		jellyfin-ffmpeg6="$ffmpeg_pkg_version-$debian" \
 		jellyfin-server="$pkg_version"
+
+	# Mark packages as dependencies, to allow automatic removal
+	apt-mark auto jellyfin-server jellyfin-web jellyfin-ffmpeg6
+}
+
+open_jellyfin_discovery_ports() {
+	discovery_service=$discovery
+	discovery_client=$discovery
+
+	if [ "$discovery" -eq 1 ]; then
+
+		# Open port $discovery_service_port for service auto-discovery
+		if ynh_port_available --port=$discovery_service_port; then
+			ynh_exec_warn_less yunohost firewall allow UDP $discovery_service_port
+		else
+			discovery_service=0
+			ynh_print_warn --message="Port $discovery_service_port (for service auto-discovery) is not available. Continuing nonetheless."
+		fi
+
+		# Open port $discovery_client_port for client auto-discovery
+		if ynh_port_available --port=$discovery_client_port; then
+			ynh_exec_warn_less yunohost firewall allow UDP $discovery_client_port
+		else
+			discovery_client=0
+			ynh_print_warn --message="Port $discovery_client_port (for client auto-discovery) is not available. Continuing nonetheless."
+		fi
+	fi
+
+	ynh_app_setting_set --app="$app" --key=discovery_service --value="$discovery_service"
+	ynh_app_setting_set --app="$app" --key=discovery_client --value="$discovery_client"
 }
 
 #=================================================
---
+++ scripts/update_version.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+from typing import Any
+from pathlib import Path
+import tomlkit
+import requests
+
+REPO_ROOT = Path(__file__).parent.parent
+
+JELLYFIN_REPO = "https://repo.jellyfin.org"
+JELLYFIN_DEB_REPO = f"{JELLYFIN_REPO}/releases/server/debian"
+
+ARCHS = [
+    "armhf",
+    "arm64",
+    "amd64",
+]
+
+DEBS = [
+    "buster",
+    "bullseye",
+    "bookworm",
+]
+
+
+def version_from__common_sh(name: str) -> str:
+    content = (REPO_ROOT/"scripts"/"_common.sh").open(encoding="utf-8").readlines()
+    result = next(filter(lambda line: line.startswith(f"{name}="), content))
+    return result.split('"')[1]
+
+
+def server_url(arch: str, version: str) -> str:
+    version_simple = version.split("-")[0]
+    return f"{JELLYFIN_DEB_REPO}/versions/stable/server/{version_simple}/jellyfin-server_{version}_{arch}.deb"
+
+
+def web_url(arch: str, version: str) -> str:
+    version_simple = version.split("-")[0]
+    return f"{JELLYFIN_DEB_REPO}/versions/stable/web/{version_simple}/jellyfin-web_{version}_all.deb"
+
+
+def ffmpeg_url(arch: str, deb: str, version: str) -> str:
+    major = version.split(".")[0]
+    return f"{JELLYFIN_DEB_REPO}/versions/jellyfin-ffmpeg/{version}/jellyfin-ffmpeg{major}_{version}-{deb}_{arch}.deb"
+
+
+def ldap_url(arch: str, version: str) -> str:
+    major = version.split(".")[0]
+    return f"{JELLYFIN_REPO}/releases/plugin/ldap-authentication/ldap-authentication_{version}.zip"
+
+
+def sha256sum_of(url: str) -> str:
+    result = requests.get(f"{url}.sha256sum", timeout=10)
+
+    content = result.content.decode("utf-8")
+    return content.split(" ")[0]
+
+
+def main() -> None:
+    manifest_file = REPO_ROOT/"manifest.toml"
+    manifest: dict[str, Any] = tomlkit.parse(manifest_file.open(encoding="utf-8").read())
+
+    jellyfin_version = version_from__common_sh("pkg_version")
+    ffmpeg_version = version_from__common_sh("ffmpeg_pkg_version")
+    ldap_version = version_from__common_sh("ldap_pkg_version")
+
+    for arch in ARCHS:
+        url = server_url(arch, jellyfin_version)
+        manifest["resources"]["sources"]["server"][arch]["url"] = url
+        manifest["resources"]["sources"]["server"][arch]["sha256"] = sha256sum_of(url)
+
+    url = web_url(arch, jellyfin_version)
+    manifest["resources"]["sources"]["web"]["url"] = url
+    manifest["resources"]["sources"]["web"]["sha256"] = sha256sum_of(url)
+
+    for arch in ARCHS:
+        for deb in DEBS:
+            url = ffmpeg_url(arch, deb, ffmpeg_version)
+            manifest["resources"]["sources"][f"ffmpeg_{deb}"][arch]["url"] = url
+            manifest["resources"]["sources"][f"ffmpeg_{deb}"][arch]["sha256"] = sha256sum_of(url)
+
+    url = ldap_url(arch, ldap_version)
+    manifest["resources"]["sources"]["plugin_ldap"]["url"] = url
+    manifest["resources"]["sources"]["plugin_ldap"]["sha256"] = sha256sum_of(url)
+
+    manifest_file.open("w", encoding="utf-8").write(tomlkit.dumps(manifest))
+
+
+if __name__ == "__main__":
+    main()
