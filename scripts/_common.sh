8234907aedeb23790c6e580e1f43a75eb55a9625
selfhoster1312 <selfhoster1312@kl.netlib.re>
2024-05-20 23:10:55 +0200

    Update to Jellfyin v10.9.2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 debian=$(lsb_release --codename --short)
 debian_number=$(lsb_release --release --short)
-pkg_version="10.9.1"
+pkg_version="10.9.2"
 version=$(echo "$pkg_version" | cut -d '-' -f 1)
 
 ffmpeg_pkg_version="6.0.1-6"
