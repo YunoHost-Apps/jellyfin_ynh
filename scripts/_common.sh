ad19e55dcfa956caca8f0bf7a77d02b46ab4052a
Kay0u <pierre@kayou.io>
2024-10-27 04:01:24 +0100

    add archive resources to update_version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -44,16 +44,16 @@ install_jellyfin_packages() {
 	tempdir="$(mktemp -d)"
 
 	# If there is a new version, the web and server resources are moved to archive
-	server_url=$"(ynh_read_manifest --manifest_key="resources.sources.server_${debian}.${YNH_ARCH}.url")"
+	server_url="$(ynh_read_manifest --manifest_key="resources.sources.server_${debian}.${YNH_ARCH}.url")"
 	server_resource="server_$debian"
-    if ! curl --output /dev/null --silent --head --fail "$server_url"; then
+	if ! curl --output /dev/null --silent --head --fail "$server_url"; then
 		server_resource="server_archive_$debian"
-    fi
-	web_url=$"(ynh_read_manifest --manifest_key="resources.sources.web_${debian}.${YNH_ARCH}.url")"
+	fi
+	web_url="$(ynh_read_manifest --manifest_key="resources.sources.web_${debian}.url")"
 	web_resource="web_$debian"
 	if ! curl --output /dev/null --silent --head --fail "$web_url"; then
 		web_resource="web_archive_$debian"
-    fi
+	fi
 	# Download the deb files
 	ynh_setup_source --dest_dir="$tempdir" --source_id="$web_resource"
 	ynh_setup_source --dest_dir="$tempdir" --source_id="ffmpeg_$debian"
