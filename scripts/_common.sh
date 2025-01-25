79cd366055d56cb03829e8fcb6b4ce9d63a1bb47
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-01-25 13:26:01 +0100

    First try to convert to helpers2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 debian=$(lsb_release --codename --short)
@@ -24,10 +24,6 @@ config_path="/etc/$app"
 log_path="/var/log/$app"
 cache_path="/var/cache/$app"
 
-#=================================================
-# PERSONAL HELPERS
-#=================================================
-
 install_jellyfin_packages() {
 	# Get version numbers from manifest UNUSED because update_version.py uses the hard-coded variables
 	# pkg_version="$(ynh_app_upstream_version)"
@@ -37,15 +33,15 @@ install_jellyfin_packages() {
 	# We need to workaround yunohost passing --no-remove to replace jellyfin-ffmpeg5 and 6...
 	for ffmpeg_installed_version in "jellyfin-ffmpeg5" "jellyfin-ffmpeg6"
 	do
-		if ynh_package_is_installed "$ffmpeg_installed_version"; then
+		if _ynh_apt_package_is_installed "$ffmpeg_installed_version"; then
 			ynh_package_remove "$ffmpeg_installed_version"
 		fi
 	done
 
 	# This should only run on upgrade, to fix https://github.com/YunoHost-Apps/jellyfin_ynh/issues/163
 	# Previously the package depended on exact package versions, so upgrade was broken.
-	if ynh_package_is_installed --package="$app-ynh-deps" && ynh_package_is_installed --package="jellyfin-server"; then
-		ynh_install_app_dependencies jellyfin-web jellyfin-ffmpeg7 jellyfin-server
+	if _ynh_apt_package_is_installed"$app-ynh-deps" && _ynh_apt_package_is_installed "jellyfin-server"; then
+		ynh_apt_install_dependencies jellyfin-web jellyfin-ffmpeg7 jellyfin-server
 	fi
 
 	# Create the temporary directory
@@ -68,18 +64,18 @@ install_jellyfin_packages() {
 	ynh_setup_source --dest_dir="$tempdir" --source_id="$server_resource"
 
 	# Install the packages. Allow downgrades because apt decided bullseye > bookworm
-	ynh_package_install --allow-downgrades \
+	_ynh_apt_install --allow-downgrades \
 		"$tempdir/jellyfin-web.deb" \
 		"$tempdir/jellyfin-server.deb"
 
 	# Install the packages. Allow downgrades because apt decided bullseye > bookworm
-	ynh_package_install --allow-downgrades \
+	_ynh_apt_install --allow-downgrades \
 		"${tempdir}/jellyfin-ffmpeg7.deb"
 
 	# The doc says it should be called only once,
 	# but the code says multiple calls are supported.
 	# Also, they're already installed so that should be quasi instantaneous.
-	ynh_install_app_dependencies jellyfin-web jellyfin-ffmpeg7 jellyfin-server
+	ynh_apt_install_dependencies jellyfin-web jellyfin-ffmpeg7 jellyfin-server
 
 	# Mark packages as dependencies, to allow automatic removal
 	apt-mark auto jellyfin-server jellyfin-web jellyfin-ffmpeg7
@@ -94,7 +90,7 @@ configure_jellyfin_discovery_ports() {
 			remove_jellyfin_discovery_ports
 			;;
 		*)
-			ynh_print_warn --message="Invalid script calling configure_jellyfin_discovery_ports with args (should be install|remove): $@"
+			ynh_print_warn "Invalid script calling configure_jellyfin_discovery_ports with args (should be install|remove): $@"
 			;;
 	esac
 }
@@ -102,12 +98,12 @@ configure_jellyfin_discovery_ports() {
 remove_jellyfin_discovery_ports() {
 	if [[ $discovery_service -eq 1 ]] && yunohost firewall list | grep -q "\- $discovery_service_port$"
 then
-	ynh_exec_warn_less yunohost firewall disallow UDP $discovery_service_port
+	ynh_hide_warnings yunohost firewall disallow UDP $discovery_service_port
 fi
 
 if [[ $discovery_client -eq 1 ]] && yunohost firewall list | grep -q "\- $discovery_client_port$"
 then
-	ynh_exec_warn_less yunohost firewall disallow UDP $discovery_client_port
+	ynh_hide_warnings yunohost firewall disallow UDP $discovery_client_port
 fi
 
 }
@@ -121,29 +117,21 @@ install_jellyfin_discovery_ports() {
 
 		# Open port $discovery_service_port for service auto-discovery
 		if ynh_port_available --port=$discovery_service_port; then
-			ynh_exec_warn_less yunohost firewall allow UDP $discovery_service_port
+			ynh_hide_warnings yunohost firewall allow UDP $discovery_service_port
 		else
 			discovery_service=0
-			ynh_print_warn --message="Port $discovery_service_port (for service auto-discovery) is not available. Continuing nonetheless."
+			ynh_print_warn "Port $discovery_service_port (for service auto-discovery) is not available. Continuing nonetheless."
 		fi
 
 		# Open port $discovery_client_port for client auto-discovery
 		if ynh_port_available --port=$discovery_client_port; then
-			ynh_exec_warn_less yunohost firewall allow UDP $discovery_client_port
+			ynh_hide_warnings yunohost firewall allow UDP $discovery_client_port
 		else
 			discovery_client=0
-			ynh_print_warn --message="Port $discovery_client_port (for client auto-discovery) is not available. Continuing nonetheless."
+			ynh_print_warn "Port $discovery_client_port (for client auto-discovery) is not available. Continuing nonetheless."
 		fi
 	fi
 
-	ynh_app_setting_set --app="$app" --key=discovery_service --value="$discovery_service"
-	ynh_app_setting_set --app="$app" --key=discovery_client --value="$discovery_client"
+	ynh_app_setting_set --key=discovery_service --value="$discovery_service"
+	ynh_app_setting_set --key=discovery_client --value="$discovery_client"
 }
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
