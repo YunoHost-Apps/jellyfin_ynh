24e4e3d73616ff1a87fd17c06af8fcbefd3da95c
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-04-26 08:51:48 +0200

    helpers2.1: remove auto-discovery

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,9 +15,6 @@ ffmpeg_pkg_version="7.0.2-9"
 plugin_abi="10.9.0.0"
 ldap_pkg_version="19.0.0.0"
 
-discovery_service_port=1900
-discovery_client_port=7359
-
 # Those directories are handled by the deb package
 data_path="/var/lib/$app"
 config_path="/etc/$app"
@@ -79,59 +76,4 @@ install_jellyfin_packages() {
 
 	# Mark packages as dependencies, to allow automatic removal
 	apt-mark auto jellyfin-server jellyfin-web jellyfin-ffmpeg7
-}
-
-configure_jellyfin_discovery_ports() {
-	case $1 in
-		"install")
-			install_jellyfin_discovery_ports
-			;;
-		"remove")
-			remove_jellyfin_discovery_ports
-			;;
-		*)
-			ynh_print_warn "Invalid script calling configure_jellyfin_discovery_ports with args (should be install|remove): $@"
-			;;
-	esac
-}
-
-remove_jellyfin_discovery_ports() {
-	if [[ $discovery_service -eq 1 ]] && yunohost firewall list | grep -q "\- $discovery_service_port$"
-then
-	ynh_hide_warnings yunohost firewall disallow UDP $discovery_service_port
-fi
-
-if [[ $discovery_client -eq 1 ]] && yunohost firewall list | grep -q "\- $discovery_client_port$"
-then
-	ynh_hide_warnings yunohost firewall disallow UDP $discovery_client_port
-fi
-
-}
-
-install_jellyfin_discovery_ports() {
-	discovery_service=$discovery
-	discovery_client=$discovery
-
-	if [ "$discovery" -eq 1 ]; then
-		opened_ports=($discovery_service_port $discovery_client_port)
-
-		# Open port $discovery_service_port for service auto-discovery
-		if ynh_port_available --port=$discovery_service_port; then
-			ynh_hide_warnings yunohost firewall allow UDP $discovery_service_port
-		else
-			discovery_service=0
-			ynh_print_warn "Port $discovery_service_port (for service auto-discovery) is not available. Continuing nonetheless."
-		fi
-
-		# Open port $discovery_client_port for client auto-discovery
-		if ynh_port_available --port=$discovery_client_port; then
-			ynh_hide_warnings yunohost firewall allow UDP $discovery_client_port
-		else
-			discovery_client=0
-			ynh_print_warn "Port $discovery_client_port (for client auto-discovery) is not available. Continuing nonetheless."
-		fi
-	fi
-
-	ynh_app_setting_set --key=discovery_service --value="$discovery_service"
-	ynh_app_setting_set --key=discovery_client --value="$discovery_client"
 }
